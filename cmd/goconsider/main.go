f887f16d9dfabe987bb1c87d51abe830233fe6f7
Christian Haas <christian.haas@sevensuns.at>
2020-08-01 22:12:55 +0200

    Add line terminator for stderr message

--- cmd/goconsider/main.go
+++ cmd/goconsider/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	err := run(os.Stdout, os.Args[1:])
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%v", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 }
